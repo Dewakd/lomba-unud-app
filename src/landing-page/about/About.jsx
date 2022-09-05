8363f3fc608627d1be198a5df9ad25c5120a6271
WUKWUK <seicrossgg@gmail.com>
2022-09-05 22:57:32 +0800

    Add Perfect AboutUS

--- src/landing-page/about/About.jsx
+++ src/landing-page/about/About.jsx
@@ -7,24 +7,22 @@ function About() {
     <div className='abt__about section__padding'>
       <div className="abt__about-content">
         <div className="abt__about-content_button">
-          <p><a href="">siapa kami?</a></p>
-          <p><a href="">seberapa bahaya perubahan iklim?</a></p>
+          <p className='kiri'><a href="">siapa kami?</a></p>
+          <p className='kanan'><a href="">seberapa bahaya perubahan iklim?</a></p>
         </div>
         
         <div className="abt__about-content_about-us section__margin">
           <div className="abt__about-content_about-us_left">
             <h1>Rawat Bumi Anda Bersama Naturesia</h1>
-            <p>Naturesia adalah komunitas yang berlokasi di Jakarta, 
-              Indonesia. 
-              Berdiri pada tahun 2013 dan secara resmi didirikan
-              pada tahun 2015. 
-              Adanya komunitas kami untuk mengatasi, membantu dan bahkan 
-              mencegah hal-hal yang akan terjadi atau belum terjadi akibat adanya
-              Perubahan Iklim. Kami
-              telah bekerja sama dengan beberapa instansi 
-              dan komunitas
-              lainnya demi mewujudkan harapan kita semua, yaitu bumi
-              dan masyarakat yang sehat.
+            <p>Naturesia adalah komunitas yang berlokasi di Jakarta, Indonesia. Berdiri pada tahun 2013 dan secara resmi didirikan
+            pada tahun 2015. 
+            Adanya komunitas kami untuk <span> mengatasi, membantu dan bahkan 
+            mencegah hal-hal yang akan terjadi atau belum terjadi akibat adanya
+            Perubahan Iklim. </span> Kami
+            telah bekerja sama dengan beberapa instansi 
+            dan komunitas
+            lainnya demi mewujudkan harapan kita semua, yaitu bumi
+            dan masyarakat yang sehat.
             </p>
           </div>
           <div className="abt__about-content_about-us_right">
