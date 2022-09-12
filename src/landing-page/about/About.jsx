fda887c6628d9a7b63cdcc76802b1f8c1ce86212
WUKWUK <seicrossgg@gmail.com>
2022-09-12 09:58:02 +0800

    a

--- src/landing-page/about/About.jsx
+++ src/landing-page/about/About.jsx
@@ -1,34 +1,54 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './about.css'
 import Bumi from '../../assets/aboutus-earth.png'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css"; 
+
+const Abouts = () => (
+  <>
+  <div className="abt__about-content_about-us section__margin">
+      <div className="abt__about-content_about-us_left">
+        <h1>Rawat Bumi Anda Bersama Naturesia</h1>
+        <p>Naturesia adalah komunitas yang berlokasi di Jakarta, Indonesia. Berdiri pada tahun 2013 dan secara resmi didirikan
+        pada tahun 2015. 
+        Adanya komunitas kami untuk <span> mengatasi, membantu dan bahkan 
+        mencegah hal-hal yang akan terjadi atau belum terjadi akibat adanya
+        Perubahan Iklim. </span> Kami
+        telah bekerja sama dengan beberapa instansi 
+        dan komunitas
+        lainnya demi mewujudkan harapan kita semua, yaitu bumi
+        dan masyarakat yang sehat.
+        </p>
+        <button>Read more about us</button>
+      </div>
+      <div className="abt__about-content_about-us_right">
+        <img src={Bumi} alt="" />
+      </div>
+    </div>
+  </>
+)
+
+const Info = () => (
+  <>
+   <div className="abt__about-content_about-info section__margin">
+
+    </div>
+  </>
+)
 
 function About() {
+  const [toggleMenu, setToggleMenu] = useState("info");
   return (
     <div className='abt__about section__padding'>
       <div className="abt__about-content">
         <div className="abt__about-content_button">
-          <p className='kiri'><a href="">siapa kami?</a></p>
-          <p className='kanan'><a href="">seberapa bahaya perubahan iklim?</a></p>
+          <p className='kiri' onClick={() => setToggleMenu("about")}>siapa kami?</p>
+          <p className='kanan' onClick={() => setToggleMenu("info")}>seberapa bahayanya perubahan iklim?</p>
         </div>
         
-        <div className="abt__about-content_about-us section__margin">
-          <div className="abt__about-content_about-us_left">
-            <h1>Rawat Bumi Anda Bersama Naturesia</h1>
-            <p>Naturesia adalah komunitas yang berlokasi di Jakarta, Indonesia. Berdiri pada tahun 2013 dan secara resmi didirikan
-            pada tahun 2015. 
-            Adanya komunitas kami untuk <span> mengatasi, membantu dan bahkan 
-            mencegah hal-hal yang akan terjadi atau belum terjadi akibat adanya
-            Perubahan Iklim. </span> Kami
-            telah bekerja sama dengan beberapa instansi 
-            dan komunitas
-            lainnya demi mewujudkan harapan kita semua, yaitu bumi
-            dan masyarakat yang sehat.
-            </p>
-          </div>
-          <div className="abt__about-content_about-us_right">
-            <img src={Bumi} alt="" />
-          </div>
-        </div>
+        {toggleMenu === "about" && <Abouts/>}
+        {toggleMenu === "info" && <Info/>}
         
       </div>
     </div>
